3d95f08c73fe18de195716a6e1cf48cb24667ae9
LinkCs5 <jesus514cauichsosa@gmail.com>
2024-03-05 19:09:34 -0600

    asdas

---
+++ Domain/Dtos/ReportDtos/ReportTypeDTO.cs
@@ -0,0 +1,11 @@
+namespace StudentHive.Domain.Dtos;
+
+public class ReportTypeDTO
+{
+    public int IdTypeReport { get; set; }
+
+    public string? TypeReportName { get; set; }
+    // public virtual ICollection<RentalHouseDto> RentalHouses { get; set; } = new List<RentalHouseDto>();
+
+    // public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
+}
